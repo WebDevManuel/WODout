fb1de0927ebca3e2968dc126110a0836262a0707
“WebDevManuel” <donmanuel2005@web.de>
2021-12-21 22:38:25 +0100

    :sparkles: Add mapping to trainingList

--- src/components/trainingList/TrainingList.tsx
+++ src/components/trainingList/TrainingList.tsx
@@ -4,32 +4,29 @@ type TrainingListProps = {
   category: string;
   movement: string;
   items: item[];
-}
+};
 
 type item = {
   sets: number;
   reps: number;
   weight: number;
-}
+};
 
-
-function TrainingList({category, movement, items}: TrainingListProps) {
+function TrainingList({ category, movement, items }: TrainingListProps) {
   return (
     <>
-    <section className={styles.container}>
-      <h2>{category}</h2>
-      <h3>{movement}</h3>
-      
-      
-      <ul>
-        <li>{items.map((item) () => (
-          <item key={ClipboardItem.value} value={interfacem.value}>
-            {item}
-            </item>
-        ))}
-        </li>
-      </ul>
-    </section>
+      <section className={styles.container}>
+        <h2>{category}</h2>
+        <h3>{movement}</h3>
+
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.sets} * {item.reps} @ {item.weight}
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
